4412349024a12e4546f0d04f1ad19bd52d7fbddb
miguel <osoazul97@gmail.com>
2016-03-07 11:10:57 -0600

    Testing First Item

---
+++ Coldcraft/src/java/com/coldteam/coldcraft/client/render/items/ItemRenderRegister.java
@@ -0,0 +1,23 @@
+package com.coldteam.coldcraft.client.render.items;
+
+import com.coldteam.coldcraft.Main;
+import com.coldteam.coldcraft.items.ModItems;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+
+public final class ItemRenderRegister {
+
+    public static void registerItemRenderer() {
+    	reg(ModItems.testItem);
+    }
+    
+    public static String modid = Main.MODID;
+
+    public static void reg(Item item) {
+        Minecraft.getMinecraft().getRenderItem().getItemModelMesher()
+        .register(item, 0, new ModelResourceLocation(modid + ":" + item.getUnlocalizedName().substring(5), "inventory"));
+    }
+    
+}
---
+++ Coldcraft/src/java/com/coldteam/coldcraft/items/BasicItem.java
@@ -0,0 +1,13 @@
+package com.coldteam.coldcraft.items;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class BasicItem extends Item {
+
+    public BasicItem(String name) {
+        super();
+        this.setUnlocalizedName(name);
+        this.setCreativeTab(CreativeTabs.tabMaterials);
+    }
+}
