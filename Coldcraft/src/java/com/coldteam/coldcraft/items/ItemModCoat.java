de1f960b482208941d8f3572f2f8eaa02fba3984
Miguel Angel <miguel.mzbi@gmail.com>
2016-05-06 14:11:49 -0500

    Comments on Items

--- Coldcraft/src/java/com/coldteam/coldcraft/items/ItemModCoat.java
+++ Coldcraft/src/java/com/coldteam/coldcraft/items/ItemModCoat.java
@@ -7,19 +7,20 @@ import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
+//Functions of the custom armor
 public class ItemModCoat extends ItemArmor{
-	
+	//Constructor
 	public ItemModCoat(String unlocalizedName, ArmorMaterial material, int renderIndex, int armorType) {
         super(material, renderIndex, armorType);
-
         this.setUnlocalizedName(unlocalizedName);
     }
-	
+	//Event that gets called on every tick for the armor and the player
 	@Override
-	public void onArmorTick(World world, EntityPlayer player, ItemStack itemStack) {
+	public void onArmorTick(World world, EntityPlayer player, ItemStack itemStack) {//If the three armor parts are being used
 		if (player.inventory.armorItemInSlot(0) != null && player.inventory.armorItemInSlot(0).getItem() == ModItems.coatBoots
 		        && player.inventory.armorItemInSlot(2) != null && player.inventory.armorItemInSlot(2).getItem() == ModItems.coatCoat
 		        && player.inventory.armorItemInSlot(3) != null && player.inventory.armorItemInSlot(3).getItem() == ModItems.winterCap) {
+				//Set to true the armor usage on tick on PlayerData for the MP
 		        if(PlayerData.get(player).isServerSide()){
 		        	PlayerData.get(player).setArmor(true);
 		        }
--- Coldcraft/src/java/com/coldteam/coldcraft/items/ModItems.java
+++ Coldcraft/src/java/com/coldteam/coldcraft/items/ModItems.java
@@ -11,30 +11,29 @@ import net.minecraft.world.World;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.common.util.EnumHelper;
 
-
+//Creation of items
 public final class ModItems {
 	
-	public static Item thermometer;
-	public static ArmorMaterial Coat = EnumHelper.addArmorMaterial("Coat", "coldcraft:coat", 6, new int[]{1, 3, 2, 1}, 0);
-	public static ItemModCoat coatBoots;
-	public static ItemModCoat coatCoat;
-	public static ItemModCoat winterCap;
+	public static Item thermometer; //New item thermometer
+	public static ArmorMaterial Coat = EnumHelper.addArmorMaterial("Coat", "coldcraft:coat", 6, new int[]{1, 3, 2, 1}, 0); //New armor coat
+	public static ItemModCoat coatBoots;//New item of coat
+	public static ItemModCoat coatCoat;//New item of coat
+	public static ItemModCoat winterCap;//New item of coat
 
 	public static void createItems() {
-		
 		//Open GUI
-		GameRegistry.registerItem(thermometer = new Item() {
+		GameRegistry.registerItem(thermometer = new Item() {//Register thermometer
 		    @Override
-		    public ItemStack onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn) {
-		        if (worldIn.isRemote) {
+		    public ItemStack onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn) {//When player right click the item on hand
+		        if (worldIn.isRemote) {//Create GUI
 		            playerIn.openGui(Main.instance, 0, worldIn, (int) playerIn.posX, (int) playerIn.posY, (int) playerIn.posZ);
 		        }
 		        return itemStackIn;
 		    }
 		}.setUnlocalizedName("thermometer").setCreativeTab(CreativeTabs.tabMisc), "thermometer");
 		
-		GameRegistry.registerItem(winterCap = new ItemModCoat("winter_cap", Coat, 1, 0), "winter_cap");
-		GameRegistry.registerItem(coatCoat = new ItemModCoat("coat_coat", Coat, 1, 1), "coat_coat");
-		GameRegistry.registerItem(coatBoots = new ItemModCoat("coat_boots", Coat, 1, 3), "coat_boots");       
+		GameRegistry.registerItem(winterCap = new ItemModCoat("winter_cap", Coat, 1, 0), "winter_cap");//Register
+		GameRegistry.registerItem(coatCoat = new ItemModCoat("coat_coat", Coat, 1, 1), "coat_coat");//Register
+		GameRegistry.registerItem(coatBoots = new ItemModCoat("coat_boots", Coat, 1, 3), "coat_boots");//Register
 	}
 }
