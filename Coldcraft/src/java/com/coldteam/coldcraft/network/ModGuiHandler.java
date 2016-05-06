2d9aa07e96671f8d47a1e93f7c947ca7b57638ba
Miguel Angel <miguel.mzbi@gmail.com>
2016-05-06 14:13:33 -0500

    Comments on Handler

--- Coldcraft/src/java/com/coldteam/coldcraft/network/ModGuiHandler.java
+++ Coldcraft/src/java/com/coldteam/coldcraft/network/ModGuiHandler.java
@@ -6,15 +6,16 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.network.IGuiHandler;
 
+//Handler for the GUI
 public class ModGuiHandler implements IGuiHandler {
 
 	public static final int TEMPERATURE_GUI = 0;
-
+	//For the server
 	@Override
 	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
 		return null;
 	}
-
+	//For the client
 	@Override
 	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
 		if (ID == TEMPERATURE_GUI)
