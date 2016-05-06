0bb96b59ef7e45cc44b29be85c210003a109282a
Miguel Angel <miguel.mzbi@gmail.com>
2016-05-06 14:29:01 -0500

    Comments on Tile Entities

--- Coldcraft/src/java/com/coldteam/coldcraft/tileentity/ModTileEntities.java
+++ Coldcraft/src/java/com/coldteam/coldcraft/tileentity/ModTileEntities.java
@@ -1,11 +1,10 @@
 package com.coldteam.coldcraft.tileentity;
 
 import net.minecraftforge.fml.common.registry.GameRegistry;
-
+//Register all tile entities
 public final class ModTileEntities {
-
+	//Register initialization
 	public static void init() {
 		GameRegistry.registerTileEntity(ModTileEntity.class, "camp_fire");
 	}
-
 }
--- Coldcraft/src/java/com/coldteam/coldcraft/tileentity/ModTileEntity.java
+++ Coldcraft/src/java/com/coldteam/coldcraft/tileentity/ModTileEntity.java
@@ -9,21 +9,23 @@ import net.minecraft.server.gui.IUpdatePlayerListBox;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
 
+//Functionality of tile entity
 public class ModTileEntity extends TileEntity implements IUpdatePlayerListBox{
-
+	//Method called on every client and server tick
 	@Override
 	public void update(){
+		//Get coordinates of tiles
 		int xCoord = this.getPos().getX();
 		int yCoord = this.getPos().getY();
 		int zCoord = this.getPos().getZ();
-				
+		//Create volume	to check on every tile
 		AxisAlignedBB aabb = AxisAlignedBB.fromBounds(xCoord-4, yCoord-4, zCoord-4, xCoord+4, yCoord+4, zCoord+4);
-		
+		//Fill list with instances of EntityPlayer in the volume
 		List<?> presentEnt = worldObj.getEntitiesWithinAABB(EntityPlayer.class, aabb);
 		if(!presentEnt.isEmpty()){
 			EntityPlayer player = (EntityPlayer) presentEnt.get(0);
-			if (PlayerData.get(player).isServerSide()){
-				PlayerData.get(player).setCamp(true);
+			if (PlayerData.get(player).isServerSide()){//If player present on server side
+				PlayerData.get(player).setCamp(true);//Set campfire calling true
 			}
 		}
 	}
