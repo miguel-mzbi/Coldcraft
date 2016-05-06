32bf73d4d2f272e03fb56bfea8a42874375382bc
Miguel Angel <miguel.mzbi@gmail.com>
2016-05-06 14:06:04 -0500

    Comments on Events Handlers

--- Coldcraft/src/java/com/coldteam/coldcraft/event/EventHandlerCommon.java
+++ Coldcraft/src/java/com/coldteam/coldcraft/event/EventHandlerCommon.java
@@ -9,22 +9,23 @@ import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+//General event manager
 public class EventHandlerCommon {
-	
+	//When entity is constructed PlayerData is registered to it
 	@SubscribeEvent
 	public void onEntityConstructing(EntityConstructing e) {
 	    if (e.entity instanceof EntityPlayer) {
 	        PlayerData.register((EntityPlayer) e.entity);
 	    }
 	}
-
+	//Event that gets called when player joins single player world. PlayerData is sync to the SP from the MP
 	@SubscribeEvent
 	public void onEntityJoinWorld(EntityJoinWorldEvent e) {
 	    if (e.entity instanceof EntityPlayer) {
 	        PlayerData.get((EntityPlayer) e.entity).requestSyncAll();
 	    }
 	}
-	
+	//When player changes of dimension, data is saved and loaded afterwards.
 	@SubscribeEvent
 	public void onPlayerCloned(PlayerEvent.Clone e) {
 	    NBTTagCompound nbt = new NBTTagCompound();
--- Coldcraft/src/java/com/coldteam/coldcraft/event/LivingUpdateEvent.java
+++ Coldcraft/src/java/com/coldteam/coldcraft/event/LivingUpdateEvent.java
@@ -6,14 +6,14 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+//Event that gets called both for the server and client entity on each tick
 public class LivingUpdateEvent {
-
 	@SubscribeEvent
-	public void onEntityUpdate(LivingEvent.LivingUpdateEvent e) {
+	public void onEntityUpdate(LivingEvent.LivingUpdateEvent e) {//If the entity which called the update is a player
 		if(e.entityLiving instanceof EntityPlayer){
 			EntityPlayer entityPlayer = (EntityPlayer) e.entityLiving;
-			if(PlayerData.get(entityPlayer).isServerSide()){
-				PlayerData.get(entityPlayer).doStuff();
+			if(PlayerData.get(entityPlayer).isServerSide()){//If the player is called from the server
+				PlayerData.get(entityPlayer).doStuff();//Call for the general loop on Player Data
 			}
 		}
 	}
