aa6d43b756a43af00204751ef5a9564e9eb1312d
Miguel Angel <miguel.mzbi@gmail.com>
2016-05-06 12:51:45 -0500

    Comments to Item Render

--- Coldcraft/src/java/com/coldteam/coldcraft/client/render/items/ItemRenderRegister.java
+++ Coldcraft/src/java/com/coldteam/coldcraft/client/render/items/ItemRenderRegister.java
@@ -8,23 +8,27 @@ import com.coldteam.coldcraft.Main;
 import com.coldteam.coldcraft.items.ItemModCoat;
 import com.coldteam.coldcraft.items.ModItems;
 
+//Register items textures
 public final class ItemRenderRegister {
 
 	public static String modid = Main.MODID;
 	
 	public static void registerItemRenderer() {
-	    reg(ModItems.thermometer);
-	    reg(ModItems.coatBoots);
-	    reg(ModItems.winterCap);
-	    reg(ModItems.coatCoat);
-
+	    reg(ModItems.thermometer);//Register thermometer textures
+	    reg(ModItems.coatBoots);//Register coat boots textures
+	    reg(ModItems.winterCap);//Register winter cap textures
+	    reg(ModItems.coatCoat);//Register coat textures
 	}
 
+	//Register method for items
 	public static void reg(Item item) {
-		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, 0, new ModelResourceLocation(modid + ":" + item.getUnlocalizedName().substring(5), "inventory"));
+		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, 0, 
+				new ModelResourceLocation(modid + ":" + item.getUnlocalizedName().substring(5), "inventory"));
 	}
 	
+	//Register method for coat's items
 	public static void reg(ItemModCoat item){
-		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, 0, new ModelResourceLocation(modid + ":" + item.getUnlocalizedName().substring(5), "inventory"));
+		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, 0, 
+				new ModelResourceLocation(modid + ":" + item.getUnlocalizedName().substring(5), "inventory"));
 	}
 }
