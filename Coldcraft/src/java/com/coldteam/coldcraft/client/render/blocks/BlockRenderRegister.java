b697bfe8aafccc318bddb212c6a0a63a671e40d4
Miguel Angel <miguel.mzbi@gmail.com>
2016-05-06 12:42:35 -0500

    Comments Block Render

--- Coldcraft/src/java/com/coldteam/coldcraft/client/render/blocks/BlockRenderRegister.java
+++ Coldcraft/src/java/com/coldteam/coldcraft/client/render/blocks/BlockRenderRegister.java
@@ -8,17 +8,18 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 
+//Register blocks textures
 public final class BlockRenderRegister {
 
 	public static void registerBlockRenderer() {
-		reg(ModBlocks.campFire);
+		reg(ModBlocks.campFire); //Register campfire texture 
 	}
 
-	public static String modid = Main.MODID;
+	public static String modid = Main.MODID; //Get ModID to assign texture location
 
 	public static void reg(Block block) {
-		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(Item.getItemFromBlock(block), 
-				0, new ModelResourceLocation(modid + ":" + block.getUnlocalizedName().substring(5), "inventory"));
+		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(Item.getItemFromBlock(block), //Get texture 
+				0, new ModelResourceLocation(modid + ":" + block.getUnlocalizedName().substring(5), "inventory"));//Get location
 	}
 }
 
